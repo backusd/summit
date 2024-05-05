5863f33dbb7d66af892d37c07de1d790a0c136cb
Dakota Backus <backusdakota@gmail.com>
2024-05-04 17:42:12 -0700

    Made RAII socket class

--- summit/src/Http.cpp
+++ summit/src/Http.cpp
@@ -7,23 +7,158 @@
 
 namespace summit
 {
+#ifdef SUMMIT_PLATFORM_WINDOWS
+void PrintAddrInfoDetails(addrinfo* addrInfo) noexcept
+{
+    LOG_INFO("\tFlags: {0}", addrInfo->ai_flags);
+    switch (addrInfo->ai_family)
+    {
+    case AF_UNSPEC:
+        LOG_INFO("\tFamily: Unspecified");
+        break;
+    case AF_INET:
+    {
+        struct sockaddr_in* sockaddr_ipv4 = (struct sockaddr_in*)addrInfo->ai_addr;
+        LOG_INFO("\tFamily: AF_INET (IPv4)");
+        LOG_INFO("\tIPv4 address {0}", inet_ntoa(sockaddr_ipv4->sin_addr));
+        break;
+    }
+    case AF_INET6:
+    {
+        LPSOCKADDR sockaddr_ip;
+        wchar_t ipstringbuffer[46];
+        DWORD ipbufferlength = 46;
+
+        LOG_INFO("\tFamily: AF_INET6 (IPv6)");
+        // the InetNtop function is available on Windows Vista and later
+        // sockaddr_ipv6 = (struct sockaddr_in6 *) addrInfo->ai_addr;
+        // LOG_INFO("\tIPv6 address %s",
+        //    InetNtop(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, 46) );
+
+        // We use WSAAddressToString since it is supported on Windows XP and later
+        sockaddr_ip = (LPSOCKADDR)addrInfo->ai_addr;
+        // The buffer length is changed by each call to WSAAddresstoString
+        // So we need to set it for each iteration through the loop for safety
+        ipbufferlength = 46;
+        int iRetval = WSAAddressToString(sockaddr_ip, (DWORD)addrInfo->ai_addrlen, NULL, ipstringbuffer, &ipbufferlength);
+        if (iRetval)
+            LOG_ERROR("\tWSAAddressToString failed with {0}", WSAGetLastError());
+        else
+        {
+            std::wstring ipsb(ipstringbuffer);
+            LOG_INFO("\tIPv6 address {0}", ws2s(ipsb));
+        }
+        break;
+    }
+    case AF_NETBIOS:
+        LOG_INFO("\tFamily: AF_NETBIOS (NetBIOS)");
+        break;
+    default:
+        LOG_INFO("\tFamily: Other {0}", addrInfo->ai_family);
+        break;
+    }
+
+    switch (addrInfo->ai_socktype)
+    {
+    case 0:
+        LOG_INFO("\tSocket type: Unspecified");
+        break;
+    case SOCK_STREAM:
+        LOG_INFO("\tSocket type: SOCK_STREAM (stream)");
+        break;
+    case SOCK_DGRAM:
+        LOG_INFO("\tSocket type: SOCK_DGRAM (datagram) ");
+        break;
+    case SOCK_RAW:
+        LOG_INFO("\tSocket type: SOCK_RAW (raw) ");
+        break;
+    case SOCK_RDM:
+        LOG_INFO("\tSocket type: SOCK_RDM (reliable message datagram)");
+        break;
+    case SOCK_SEQPACKET:
+        LOG_INFO("\tSocket type: SOCK_SEQPACKET (pseudo-stream packet)");
+        break;
+    default:
+        LOG_INFO("\tSocket type: Other {0}", addrInfo->ai_socktype);
+        break;
+    }
+
+    switch (addrInfo->ai_protocol)
+    {
+    case 0:
+        LOG_INFO("\tProtocol: Unspecified");
+        break;
+    case IPPROTO_TCP:
+        LOG_INFO("\tProtocol: IPPROTO_TCP (TCP)");
+        break;
+    case IPPROTO_UDP:
+        LOG_INFO("\tProtocol: IPPROTO_UDP (UDP) ");
+        break;
+    default:
+        LOG_INFO("\tProtocol: Other {0}", addrInfo->ai_protocol);
+        break;
+    }
+    LOG_INFO("\tLength of this sockaddr: {0}", addrInfo->ai_addrlen);
+    LOG_INFO("\tCanonical name: {0}", addrInfo->ai_canonname);
+}
+#endif
+
+// ==========================================================================================
+// Socket
+Socket::Socket(Socket::Type type, Protocol protocol) :
+    m_socket(0)
+{
+#ifdef SUMMIT_PLATFORM_WINDOWS
+    int _af = AF_INET;
+    int _type, _protocol;
+
+    switch (type)
+    {
+    case Socket::Type::STREAM:      _type = SOCK_STREAM; break;
+    case Socket::Type::DGRAM:       _type = SOCK_DGRAM; break;
+    case Socket::Type::RAW:         _type = SOCK_RAW; break;
+    case Socket::Type::RDM:         _type = SOCK_RDM; break;
+    case Socket::Type::SEQPACKET:   _type = SOCK_SEQPACKET; break;
+    default:
+        throw EXCEPTION(std::format("Uknown socket type: {0}", type));
+    }
+
+    switch (protocol)
+    {
+    case Protocol::TCP: _protocol = IPPROTO_TCP; break;
+    case Protocol::UDP: _protocol = IPPROTO_UDP; break;
+    default:
+        throw EXCEPTION(std::format("Uknown protocol type: {0}", type));
+    }
+
+    m_socket = socket(_af, _type, _protocol);
+#endif
+}
+
+Socket::~Socket()
+{
+#ifdef SUMMIT_PLATFORM_WINDOWS
+    closesocket(m_socket);
+#endif
+}
+
+// ==========================================================================================
+// Http
 Http::Http()
 {
 #ifdef SUMMIT_PLATFORM_WINDOWS
 	WSADATA wsaData = {};
     int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (err != 0) 
-    {
+    if (err != 0)
         throw EXCEPTION(std::format("WSAStartup failed with error: {0}", err));
-    }
 
     // Confirm that the WinSock DLL supports 2.2.
     // Note that if the DLL supports versions greater   
     // than 2.2 in addition to 2.2, it will still return
     // 2.2 in wVersion since that is the version we     
     // requested.                                       
-
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) 
+    {
         // Could not find a usable WinSock DLL.     
         WSACleanup();
         throw EXCEPTION("Could not find a usable version of Winsock.dll");
@@ -126,9 +261,9 @@ std::string Http::GetImpl(std::string_view url) const
             break;
         }
     }
-    
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (connect(sock, sockaddr_ipv4, sizeof(sockaddr_in)) != 0) 
+
+    Socket sock(Socket::Type::STREAM, Protocol::TCP);
+    if (connect(sock.Get(), sockaddr_ipv4, sizeof(sockaddr_in)) != 0)
         throw EXCEPTION(std::format("Failed to connect to: {0}.\r\nWSAGetLastError: {1}", url, WSAGetLastError()));
 
     size_t sent = 0;
@@ -136,7 +271,7 @@ std::string Http::GetImpl(std::string_view url) const
     while (remaining > 0)
     {
         const char* data = request.c_str() + sent;
-        auto result = send(sock, data, remaining, 0); 
+        auto result = send(sock.Get(), data, remaining, 0);
         if (result == SOCKET_ERROR)
             throw EXCEPTION(std::format("Failed to send all data to: {0}.\r\nWSAGetLastError: {1}", url, WSAGetLastError()));
         sent += result;
@@ -146,7 +281,7 @@ std::string Http::GetImpl(std::string_view url) const
     char buffer[10000];
 
     int nDataLength;
-    while ((nDataLength = recv(sock, buffer, 10000, 0)) > 0)
+    while ((nDataLength = recv(sock.Get(), buffer, 10000, 0)) > 0)
     {
         // TODO: There has to be a better method here than push_back
         for (int iii = 0; iii < nDataLength; ++iii)
@@ -154,106 +289,11 @@ std::string Http::GetImpl(std::string_view url) const
     }
 
     // NOTE: We are throwing exceptions above and not calling these functions - implement RAII
-    closesocket(sock);
     freeaddrinfo(addrInfo);
 
 #endif
     return result;
 }
 
-#ifdef SUMMIT_PLATFORM_WINDOWS
-void Http::PrintAddrInfoDetails(addrinfo* addrInfo) const noexcept
-{
-    LOG_INFO("\tFlags: {0}", addrInfo->ai_flags);
-    switch (addrInfo->ai_family)
-    {
-    case AF_UNSPEC:
-        LOG_INFO("\tFamily: Unspecified");
-        break;
-    case AF_INET:
-    {
-        struct sockaddr_in* sockaddr_ipv4 = (struct sockaddr_in*)addrInfo->ai_addr;
-        LOG_INFO("\tFamily: AF_INET (IPv4)");
-        LOG_INFO("\tIPv4 address {0}", inet_ntoa(sockaddr_ipv4->sin_addr));
-        break;
-    }
-    case AF_INET6:
-    {
-        LPSOCKADDR sockaddr_ip;
-        wchar_t ipstringbuffer[46];
-        DWORD ipbufferlength = 46;
 
-        LOG_INFO("\tFamily: AF_INET6 (IPv6)");
-        // the InetNtop function is available on Windows Vista and later
-        // sockaddr_ipv6 = (struct sockaddr_in6 *) addrInfo->ai_addr;
-        // LOG_INFO("\tIPv6 address %s",
-        //    InetNtop(AF_INET6, &sockaddr_ipv6->sin6_addr, ipstringbuffer, 46) );
-
-        // We use WSAAddressToString since it is supported on Windows XP and later
-        sockaddr_ip = (LPSOCKADDR)addrInfo->ai_addr;
-        // The buffer length is changed by each call to WSAAddresstoString
-        // So we need to set it for each iteration through the loop for safety
-        ipbufferlength = 46;
-        int iRetval = WSAAddressToString(sockaddr_ip, (DWORD)addrInfo->ai_addrlen, NULL, ipstringbuffer, &ipbufferlength);
-        if (iRetval)
-            LOG_ERROR("\tWSAAddressToString failed with {0}", WSAGetLastError());
-        else
-        {
-            std::wstring ipsb(ipstringbuffer);
-            LOG_INFO("\tIPv6 address {0}", ws2s(ipsb));
-        }
-        break;
-    }
-    case AF_NETBIOS:
-        LOG_INFO("\tFamily: AF_NETBIOS (NetBIOS)");
-        break;
-    default:
-        LOG_INFO("\tFamily: Other {0}", addrInfo->ai_family);
-        break;
-    }
-
-    switch (addrInfo->ai_socktype)
-    {
-    case 0:
-        LOG_INFO("\tSocket type: Unspecified");
-        break;
-    case SOCK_STREAM:
-        LOG_INFO("\tSocket type: SOCK_STREAM (stream)");
-        break;
-    case SOCK_DGRAM:
-        LOG_INFO("\tSocket type: SOCK_DGRAM (datagram) ");
-        break;
-    case SOCK_RAW:
-        LOG_INFO("\tSocket type: SOCK_RAW (raw) ");
-        break;
-    case SOCK_RDM:
-        LOG_INFO("\tSocket type: SOCK_RDM (reliable message datagram)");
-        break;
-    case SOCK_SEQPACKET:
-        LOG_INFO("\tSocket type: SOCK_SEQPACKET (pseudo-stream packet)");
-        break;
-    default:
-        LOG_INFO("\tSocket type: Other {0}", addrInfo->ai_socktype);
-        break;
-    }
-
-    switch (addrInfo->ai_protocol)
-    {
-    case 0:
-        LOG_INFO("\tProtocol: Unspecified");
-        break;
-    case IPPROTO_TCP:
-        LOG_INFO("\tProtocol: IPPROTO_TCP (TCP)");
-        break;
-    case IPPROTO_UDP:
-        LOG_INFO("\tProtocol: IPPROTO_UDP (UDP) ");
-        break;
-    default:
-        LOG_INFO("\tProtocol: Other {0}", addrInfo->ai_protocol);
-        break;
-    }
-    LOG_INFO("\tLength of this sockaddr: {0}", addrInfo->ai_addrlen);
-    LOG_INFO("\tCanonical name: {0}", addrInfo->ai_canonname);
-}
-#endif
 }
--- summit/src/Http.h
+++ summit/src/Http.h
@@ -7,6 +7,44 @@
 
 namespace summit
 {
+enum class Protocol
+{
+	TCP,
+	UDP
+};
+
+class Socket
+{
+public:
+	enum class Type
+	{
+		STREAM = 1,
+		DGRAM = 2,
+		RAW = 3,
+		RDM = 4,
+		SEQPACKET = 5
+	};
+	Socket(Socket::Type type, Protocol protocol);
+	~Socket();
+
+
+#ifdef SUMMIT_PLATFORM_WINDOWS
+	ND inline SOCKET Get() const noexcept { return m_socket; }
+#endif
+
+private:
+	// No use case yet for these
+	Socket(const Socket&) = delete;
+	Socket(Socket&&) = delete;
+	Socket& operator=(const Socket&) = delete;
+	Socket& operator=(Socket&&) = delete;
+	
+
+#ifdef SUMMIT_PLATFORM_WINDOWS
+	SOCKET m_socket;
+#endif
+};
+
 class Http
 {
 public:
@@ -27,9 +65,45 @@ private:
 
 
 	ND std::string GetImpl(std::string_view url) const;
-
-#ifdef SUMMIT_PLATFORM_WINDOWS
-	void PrintAddrInfoDetails(addrinfo* addrInfo) const noexcept;
-#endif
 };
 }
+
+
+template <>
+struct std::formatter<summit::Socket::Type> : std::formatter<std::string> {
+	auto format(summit::Socket::Type type, format_context& ctx) const
+	{
+		std::string s;
+		switch (type)
+		{
+		case summit::Socket::Type::STREAM:      s = "STREAM"; break;
+		case summit::Socket::Type::DGRAM:       s = "DGRAM"; break;
+		case summit::Socket::Type::RAW:         s = "RAW"; break;
+		case summit::Socket::Type::RDM:         s = "RDM"; break;
+		case summit::Socket::Type::SEQPACKET:   s = "SEQPACKET"; break;
+		default:
+			s = "(Unknown)"; 
+			break;
+		}
+
+		return formatter<string>::format(s, ctx);
+	}
+};
+
+template <>
+struct std::formatter<summit::Protocol> : std::formatter<std::string> {
+	auto format(summit::Protocol proto, format_context& ctx) const
+	{
+		std::string s;
+		switch (proto)
+		{
+		case summit::Protocol::TCP: s = "TCP"; break;
+		case summit::Protocol::UDP: s = "UDP"; break;
+		default:
+			s = "(Unknown)";
+			break;
+		}
+
+		return formatter<string>::format(s, ctx);
+	}
+};
